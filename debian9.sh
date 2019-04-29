2241b9e323e70c7f6b32853d3d9ffbd62fef4bb4
phisanu <phisanuphoca@gmail.com>
2019-04-29 21:20:15 +0700

    jj

--- debian9.sh
+++ debian9.sh
@@ -5,33 +5,6 @@
 # 
 # ==================================================
 
-if [ $USER != 'root' ]; then
-	echo "You must run this as root"
-	exit
-fi
-
-# initialisasi var
-export DEBIAN_FRONTEND=noninteractive
-OS=`uname -m`;
-
-if [[ -e /etc/debian_version ]]; then
-	#OS=debian
-	RCLOCAL='/etc/rc.local'
-else
-	echo "You are not running this script on Debian OS"
-	exit
-fi
-
-vps="vps";
-
-if [[ $vps = "vps" ]]; then
-	source="https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master"
-else
-	source="https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master"
-fi
-
-# go to root
-cd
 
 echo "You are not running this script on Debian OS"
 echo "You are not running this script on Debian OS"
